cbc8a1b4638d809d8a2f6f582d68e16d1fd0c582
T_inoue_T <t.artwork.ceramica@gmail.com>
2021-01-10 08:01:53 +0900

    add: 自動計算の機能

--- app/javascript/packs/materials/form.js
+++ app/javascript/packs/materials/form.js
@@ -1,13 +1,24 @@
 'use strict'
-{
+window.onload = function() {
   console.log('hello');
-  const input = document.querySelectorAll('.form__control_molratio');
-  const log = document.getElementById('sum_value');
+  // const trs = document.querySelectorAll('.__tr');
+  // console.log(trs);
+  const inputs = document.querySelectorAll('.form__control_molratio');
+  const inputSum = document.getElementById('inputs_sum');
+  let sum = 0;
+  const adder = input => sum += parseFloat(input.value);
 
-  for (let i = 0; i < input.length; i++) {
-    input[i].addEventListener('input', updateValue);
-    function updateValue(e) {
-    log.textContent = e.target.value;
-    }
-  }
+  inputs.forEach(adder);
+  inputSum.textContent = sum;
+
+  // インプットした時の合計値を算出するイベント発動
+  inputs.forEach((input) => {
+    input.addEventListener('input', () => {
+      // console.log('input');
+      sum = 0;
+      // sum += Number(input.value);
+      inputs.forEach(adder);
+      inputSum.textContent = Math.round(sum * 100) / 100; //少数点第三位で切り上げ
+    });
+  });
 }
