359c24e7ae92cf5d222545f6b5f0e6ed22b77135
T_inoue_T <t.artwork.ceramica@gmail.com>
2021-01-19 12:37:19 +0900

    fix: for文をeach文に変更

--- app/javascript/javascripts/table.js
+++ app/javascript/javascripts/table.js
@@ -1,12 +1,24 @@
 {
-  const trs = document.querySelectorAll('.__tr');
-  for (let i = 1; i < trs.length; i++) {
-    // const tr = trs[i];
-    trs[i].addEventListener('mouseover', () => {
-      trs[i].classList.add('tr-lighting');
+  let trs = document.querySelectorAll('.__tr');
+  // console.log(trs);
+
+  trs.forEach((tr, i) => {
+    if( i === 0 ){ return; } // １行目は無効にする
+    tr.addEventListener('mouseover', () => {
+      tr.classList.add('tr-lighting');
     });
-    trs[i].addEventListener('mouseout', () => {
-      trs[i].classList.remove('tr-lighting');
+    tr.addEventListener('mouseout', () => {
+      tr.classList.remove('tr-lighting');
     });
-  }
+  });
+
+  // for (let i = 1; i < trs.length; i++) {
+  //   // const tr = trs[i];
+  //   trs[i].addEventListener('mouseover', () => {
+  //     trs[i].classList.add('tr-lighting');
+  //   });
+  //   trs[i].addEventListener('mouseout', () => {
+  //     trs[i].classList.remove('tr-lighting');
+  //   });
+  // }
 }
