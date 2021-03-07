1ab6a024eb8d8136f8e667561031e7e6ae719be2
T_inoue_T <t.artwork.ceramica@gmail.com>
2021-03-07 17:47:40 +0900

    add: belongs_to :atomic_group

--- app/models/periodic_atom.rb
+++ app/models/periodic_atom.rb
@@ -1,13 +1,9 @@
 class PeriodicAtom < ApplicationRecord
   has_one :electron_shell
-  belongs_to :atomic_group
+  belongs_to :atomic_group # presence: trueを含む
 
   validates :name, presence: true, uniqueness: true
   validates :symbol, presence: true, uniqueness: true
   validates :atomic_num, presence: true, uniqueness: true
-  # validates :group, presence: true
   validates :period, presence: true
-  # validates :weight
-
-  # belongs_to :electron_shell, foreign_key: :electron_shell_id
 end
